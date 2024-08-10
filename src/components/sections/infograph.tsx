6e05ffdf814ad2cbac67985797ea427b82ab54ea
Danny Hagenlocker <dannyhagenlocker@gmail.com>
2024-08-10 02:04:26 -0400

    fix resume buttons

--- src/components/sections/infograph.tsx
+++ src/components/sections/infograph.tsx
@@ -1,3 +1,4 @@
+import Button from "../button";
 import ExperienceCard from "../experienceCard";
 
 export default function Infograph() {
@@ -18,6 +19,9 @@ export default function Infograph() {
         </h1>
         <div className="mb-8">
           <Experience />
+          <div className="mt-4">
+            <Button text="View Full Resumé" xlink="/resume.pdf" />
+          </div>
         </div>
       </div>
 
@@ -26,6 +30,9 @@ export default function Infograph() {
       </h1>
       <div>
         <Projects />
+        <div className="mt-4">
+          <Button text="View All Projects" xlink="/under-construction" />
+        </div>
       </div>
     </div>
   );
@@ -128,33 +135,6 @@ Competition (2nd Place)"
         image="logos/recycleright_logo.png"
         xlink="https://www.youtube.com/watch?v=oEOv32Nhnbc"
       />
-
-      <div className="mt-8">
-        <a
-          className="group inline-flex items-center font-semibold leading-tight text-slate-200"
-          href="/under-construction"
-        >
-          <span>
-            <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
-              View All Projects
-            </span>
-            <span className="whitespace-nowrap">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-2 group-focus-visible:translate-x-2"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </span>
-          </span>
-        </a>
-      </div>
     </>
   );
 }
--- src/components/socialBar.tsx
+++ src/components/socialBar.tsx
@@ -5,7 +5,6 @@ import {
   BsChatDots,
   BsInfoLg,
 } from "react-icons/bs";
-import resume from "../assets/Hagenlocker-Daniel_Resume copy.pdf";
 
 export default function SocialBar() {
   return (
@@ -41,14 +40,6 @@ export default function SocialBar() {
         className="basis-8"
       >
         <BsChatDots className="h-6 w-6 transition-colors hover:text-slate-200" />
-      </a>
-      <a
-        href={resume}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="basis-10"
-      >
-        <BsInfoLg className="h-6 w-6 transition-colors hover:text-slate-200" />
       </a>
     </div>
   );
