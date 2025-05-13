36ffdc220ee7a11cb5f1f8c4949f0cb21ce409f9
Danny Hagenlocker <dannyhagenlocker@gmail.com>
2025-05-13 15:15:42 -0700

    update document titles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,10 @@
 import Lanyard from "../components/lanyard";
 import Hero from "../components/sections/hero";
 import Infograph from "../components/sections/infograph";
+import { DocumentTitle } from "../utils/utils";
+
 export default function Home() {
+  DocumentTitle("Home | Danny Hagenlocker");
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 md:px-12 lg:px-24">
       <div className="fixed inset-0 hidden max-h-screen md:block">
--- src/pages/life.tsx
+++ src/pages/life.tsx
@@ -2,7 +2,7 @@ import { DocumentTitle } from "../utils/utils";
 import "../components/loader.css";
 
 export default function Life() {
-  DocumentTitle("🚧 This page is under construction 🚧");
+  DocumentTitle("Life | Danny Hagenlocker");
   return (
     <div className="mx-auto flex min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="m-auto flex w-min flex-row">
