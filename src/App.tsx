3df3561d3505e8d066eb221b001b002f59119d73
Danny Hagenlocker <dannyhagenlocker@gmail.com>
2025-05-15 15:08:01 -0700

    add page animations, still bug with 2x switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import TabSwitcher from "./pages/tabSwitcher";
 import Home from "./pages/home";
-import Spotlight from "./components/spotlight";
-import { Routes, Route } from "react-router-dom";
-import UnderConstruction from "./pages/underConstruction";
-import NotFound from "./pages/notFound";
-import TabSwitcher from "./pages/TabSwitcher";
 import Projects from "./pages/projects";
 import Life from "./pages/life";
+import UnderConstruction from "./pages/underConstruction";
+import NotFound from "./pages/notFound";
+import PageWrapper from "./components/pageWrapper";
+import { useTransitionDirection } from "./hooks/useDirection";
+import Spotlight from "./components/spotlight";
 
 function App() {
+  const location = useLocation();
+  const direction = useTransitionDirection();
+
   return (
     <>
       <Spotlight />
       <TabSwitcher />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/life" element={<Life />} />
-        <Route path="/under-construction" element={<UnderConstruction />} />
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path="/"
+            element={
+              <PageWrapper direction={direction}>
+                <Home />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/projects"
+            element={
+              <PageWrapper direction={direction}>
+                <Projects />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/life"
+            element={
+              <PageWrapper direction={direction}>
+                <Life />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="/under-construction"
+            element={
+              <PageWrapper direction={direction}>
+                <UnderConstruction />
+              </PageWrapper>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <PageWrapper direction={direction}>
+                <NotFound />
+              </PageWrapper>
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
